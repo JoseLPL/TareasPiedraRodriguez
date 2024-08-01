b40064c468c3a7fa74adacf26bbd61f7cabfcadc
Adrove <79154305+Adrove@users.noreply.github.com>
2024-08-01 16:42:35 -0600

    Update tarea_1_testing.py

--- Tarea_1/tarea_1_testing.py
+++ Tarea_1/tarea_1_testing.py
@@ -1,4 +1,4 @@
-import tarea_1_example_solution
+import Tarea1PartePractica
 import random
 import string
 
@@ -20,36 +20,36 @@ import string
 # Verifica todos los casos de error de la solución
 def test_casos_error_operation_selector():
     # Error por operando num1 y num2 erróneos
-    estado, res1 = tarea_1_example_solution.operation_selector(7, 7.1, "+")
+    estado, res1 = Tarea1PartePractica.operation_selector(7, 7.1, "+")
     assert estado == -50
     assert res1 is None
-    estado, res1 = tarea_1_example_solution.operation_selector(12, "s", "*")
+    estado, res1 = Tarea1PartePractica.operation_selector(12, "s", "*")
     assert estado == -50
     assert res1 is None
-    estado, res1 = tarea_1_example_solution.operation_selector(True, 7, "-")
+    estado, res1 = Tarea1PartePractica.operation_selector(True, 7, "-")
     assert estado == -50
     assert res1 is None
-    estado, res1 = tarea_1_example_solution.operation_selector(None, 77, "&")
+    estado, res1 = Tarea1PartePractica.operation_selector(None, 77, "&")
     assert estado == -50
     assert res1 is None
 
     # Error por operando op no string
-    estado, res1 = tarea_1_example_solution.operation_selector(
+    estado, res1 = Tarea1PartePractica.operation_selector(
         10, 10, 3)
     assert estado == -60
     assert res1 is None
-    estado, res1 = tarea_1_example_solution.operation_selector(
+    estado, res1 = Tarea1PartePractica.operation_selector(
         10, 10, 3.7)
     assert estado == -60
     assert res1 is None
-    estado, res1 = tarea_1_example_solution.operation_selector(
+    estado, res1 = Tarea1PartePractica.operation_selector(
         10, 10, True)
     assert estado == -60
     assert res1 is None
 
     # Error por operando op distinto a +, -, *, &
     random_letter = random.choice(string.ascii_letters)
-    estado, res1 = tarea_1_example_solution.operation_selector(
+    estado, res1 = Tarea1PartePractica.operation_selector(
         10, 10, random_letter)
     assert estado == -70
     assert res1 is None
@@ -62,22 +62,22 @@ def test_casos_exito_operation_selector():
     num2 = random.randint(1, 10)
 
     res_esperado = num1+num2
-    estado, res1 = tarea_1_example_solution.operation_selector(num1, num2, "+")
+    estado, res1 = Tarea1PartePractica.operation_selector(num1, num2, "+")
     assert estado == 0
     assert res1 == res_esperado
 
     res_esperado = num1-num2
-    estado, res1 = tarea_1_example_solution.operation_selector(num1, num2, "-")
+    estado, res1 = Tarea1PartePractica.operation_selector(num1, num2, "-")
     assert estado == 0
     assert res1 == res_esperado
 
     res_esperado = num1*num2
-    estado, res1 = tarea_1_example_solution.operation_selector(num1, num2, "*")
+    estado, res1 = Tarea1PartePractica.operation_selector(num1, num2, "*")
     assert estado == 0
     assert res1 == res_esperado
 
     res_esperado = num1 & num2
-    estado, res1 = tarea_1_example_solution.operation_selector(num1, num2, "&")
+    estado, res1 = Tarea1PartePractica.operation_selector(num1, num2, "&")
     assert estado == 0
     assert res1 == res_esperado
 
@@ -86,16 +86,16 @@ def test_casos_exito_operation_selector():
 # Verifica los casos de error de la funcion de numeros primos
 def test_casos_error_calculo_promedio():
     # Error por elemento no número
-    estado, result = tarea_1_example_solution.calculo_promedio([1, 2, 3, "4"])
+    estado, result = Tarea1PartePractica.calculo_promedio([1, 2, 3, "4"])
     assert estado == -80
     assert result is None
 
-    estado, result = tarea_1_example_solution.calculo_promedio([1, True, 3, 4])
+    estado, result = Tarea1PartePractica.calculo_promedio([1, True, 3, 4])
     assert estado == -80
     assert result is None
 
     # Error por tamaño de lista
-    estado, result = tarea_1_example_solution.calculo_promedio(
+    estado, result = Tarea1PartePractica.calculo_promedio(
         [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
     assert estado == -90
     assert result is None
@@ -110,6 +110,6 @@ def test_casos_calculo_promedio():
 
     promedio = sum(lista_valores)/len(lista_valores)
 
-    estado, result = tarea_1_example_solution.calculo_promedio(lista_valores)
+    estado, result = Tarea1PartePractica.calculo_promedio(lista_valores)
     assert estado == 0
     assert result == promedio
